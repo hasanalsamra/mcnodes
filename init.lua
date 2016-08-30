10abc613cd4a4034e4cf93abc8f6dab9ea2a5e1f
hasanalsamra <hasanalsamra200@gmail.com>
2016-08-30 14:39:13 +0300

    Push new version changes to init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,18 @@
+mcnodes = {}
+
+mcnodes.version = '0.3-beta'
+
+LargeChest = '1'
+
 AutoStairs = '1'
+
+McMobs = '1'
+McMobsInCreative = '0'
+
+McHud = '1'
+
+McHunger = '1'
+McHungerInCreative = '1'
 
 dofile(minetest.get_modpath("mcnodes").."/functions.general.lua")
 
@@ -15,15 +29,36 @@ dofile(minetest.get_modpath("mcnodes").."/crafting.lua")
 
 --dofile(minetest.get_modpath("mcnodes").."/hand.lua")
 
-dofile(minetest.get_modpath("mcnodes").."/chest.lua")
+if LargeChest == '1' then
+	dofile(minetest.get_modpath("mcnodes").."/chest.functions.lua")
+	dofile(minetest.get_modpath("mcnodes").."/chest.lua")
+end
 
 dofile(minetest.get_modpath("mcnodes").."/register_ores.lua")
 dofile(minetest.get_modpath("mcnodes").."/register_biome.lua")
 
 dofile(minetest.get_modpath("mcnodes").."/replaced_nodes.lua")
 
+dofile(minetest.get_modpath("mcnodes").."/chatcommands.lua")
+
 
 if AutoStairs == '1' then
 	dofile(minetest.get_modpath("mcnodes").."/auto_stairs_functions.lua")
 	dofile(minetest.get_modpath("mcnodes").."/auto_stairs.lua")
+end
+
+
+
+if McMobs == '1' then
+	if McMobsInCreative ~= '1' then
+		if not minetest.setting_getbool("creative_mode") then
+			dofile(minetest.get_modpath("mcnodes").."/mcmobs/mcmobs.init.lua")
+		end
+	else
+		dofile(minetest.get_modpath("mcnodes").."/mcmobs/mcmobs.init.lua")
+	end
+end
+
+if McHud == '1' then
+	dofile(minetest.get_modpath("mcnodes").."/mchud/mchud.init.lua")
 end
