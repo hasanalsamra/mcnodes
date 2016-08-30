b1f9cdb79f788302d6478a0218d2393996d8dd86
hasanalsamra <hasanalsamra200@gmail.com>
2016-08-30 14:40:18 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,14 +6,6 @@ LargeChest = '1'
 
 AutoStairs = '1'
 
-McMobs = '1'
-McMobsInCreative = '0'
-
-McHud = '1'
-
-McHunger = '1'
-McHungerInCreative = '1'
-
 dofile(minetest.get_modpath("mcnodes").."/functions.general.lua")
 
 dofile(minetest.get_modpath("mcnodes").."/nodes.lua")
@@ -45,20 +37,4 @@ dofile(minetest.get_modpath("mcnodes").."/chatcommands.lua")
 if AutoStairs == '1' then
 	dofile(minetest.get_modpath("mcnodes").."/auto_stairs_functions.lua")
 	dofile(minetest.get_modpath("mcnodes").."/auto_stairs.lua")
-end
-
-
-
-if McMobs == '1' then
-	if McMobsInCreative ~= '1' then
-		if not minetest.setting_getbool("creative_mode") then
-			dofile(minetest.get_modpath("mcnodes").."/mcmobs/mcmobs.init.lua")
-		end
-	else
-		dofile(minetest.get_modpath("mcnodes").."/mcmobs/mcmobs.init.lua")
-	end
-end
-
-if McHud == '1' then
-	dofile(minetest.get_modpath("mcnodes").."/mchud/mchud.init.lua")
 end
